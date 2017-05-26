d01cdc92dc892eb70e62222f9257a35aeb5af488
Sarah Fogarty <sarahefogarty@gmail.com>
2017-05-26 16:31:42 -0700

    initial commit

---
+++ collection view/FlickrPhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoCell.swift
+//  collection view
+//
+//  Created by Sarah Fogarty on 3/23/17.
+//  Copyright © 2017 Sarah Fogarty. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+}
